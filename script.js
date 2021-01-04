3396f5afafd00b2bcd6ec11b21b8e5f6bba12856
Sisnando Junior <nando.devs@gmail.com>
2021-01-04 19:28:53 -0300

    definir direções e impedir que a cobrinha suma

--- script.js
+++ script.js
@@ -22,7 +22,20 @@ function createSnake(){
     }
 }
 
+/* Evento de teclas*/
+document.addEventListener('keydown', update);
+
+function update(event){
+    if(event.keyCode == 37 && direction != "right") direction = "left"; 
+    if(event.keyCode == 38 && direction != "down") direction = "up";
+    if(event.keyCode == 39 && direction != "left") direction = "right";
+    if(event.keyCode == 40 && direction != "up") direction = "down";
+}
+
 function startGame(){
+
+    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+
     criarBG();
     createSnake();
 
@@ -35,6 +48,13 @@ function startGame(){
     if(direction == "down") snakeX += box;
 
     snake.pop();
+    
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
 }
 
 let jogo = setInterval(startGame, 100);
